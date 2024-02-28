9a55243457c1e7d580b9c14c88d15c8f1d806c90
Daniel Rinehart <danielr@neophi.com>
2024-02-28 06:18:46 -0500

    remove non cache overhead

--- src/ops.ts
+++ src/ops.ts
@@ -6,21 +6,34 @@ async function exercise(name: CacheName) {
   const file = readline.createInterface({
     input: process.stdin,
   });
-  const start = hrtime.bigint();
+
+  let getCount = 0;
+  let getTotal = 0n;
+  let setCount = 0;
+  let setTotal = 0n;
   const cache = CACHES[name](parseCacheSize());
   for await (const key of file) {
     if (key) {
+      getCount += 1;
+      const getStart = hrtime.bigint();
       const value = cache.get(key);
+      const getEnd = hrtime.bigint();
+      getTotal += getEnd - getStart;
       if (!value) {
+        setCount += 1;
+        const setStart = hrtime.bigint();
         cache.set(key, key);
+        const setEnd = hrtime.bigint();
+        setTotal += setEnd - setStart;
       }
     }
   }
-  const end = hrtime.bigint();
-  const nanoseconds = end - start;
   console.table({
     name,
-    nanoseconds,
+    getCount,
+    getNanoseconds: getTotal.toLocaleString(),
+    setCount,
+    setNanoseconds: setTotal.toLocaleString(),
   });
 }
 
