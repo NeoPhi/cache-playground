bca8aa50754d1e813746dc42fb78f91582bddec4
Daniel Rinehart <danielr@neophi.com>
2024-03-15 14:54:32 -0400

    add first ops summary

---
+++ src/ops-summary.ts
@@ -0,0 +1,90 @@
+import { readFileSync } from "fs";
+
+const lines = readFileSync("./ops.ldjson").toString().split("\n");
+const columns = {
+  workload: ":---",
+  cacheSize: "---:",
+  cacheName: ":---:",
+  getMean: "---:",
+  setMean: "---:",
+  totalTime: "---:",
+  percentOfMaxTotalTime: "---:",
+};
+const columnNames = Object.keys(columns);
+console.log(`|${columnNames.join("|")}|`);
+console.log(`|${Object.values(columns).join("|")}|`);
+const results: any = {};
+lines.forEach((line) => {
+  if (line) {
+    const {
+      workload,
+      cacheSize,
+      cacheName,
+      get: { count: getCount, mean: getMean },
+      set: { count: setCount, mean: setMean },
+    } = JSON.parse(line);
+    if (!results[workload]) {
+      results[workload] = {};
+    }
+    if (!results[workload][cacheSize]) {
+      results[workload][cacheSize] = {};
+    }
+    if (!results[workload][cacheSize][cacheName]) {
+      results[workload][cacheSize][cacheName] = {
+        resultCount: 0,
+        getCount,
+        getMeanTotal: 0,
+        setCount,
+        setMeanTotal: 0,
+      };
+    }
+    const data = results[workload][cacheSize][cacheName];
+    data.resultCount += 1;
+    data.getMeanTotal += getMean;
+    data.setMeanTotal += setMean;
+  }
+});
+Object.keys(results)
+  .sort()
+  .forEach((workload, index) => {
+    if (index >= 1) {
+      console.log(`|**${columnNames.join("**|**")}**|`);
+    }
+    const cacheSizeResults = results[workload];
+    Object.keys(cacheSizeResults)
+      .sort((a, b) => parseInt(a, 10) - parseInt(b, 10))
+      .forEach((cacheSizeString) => {
+        const cacheNameResults = cacheSizeResults[cacheSizeString];
+        const group: any[] = [];
+        Object.keys(cacheNameResults)
+          .sort()
+          .forEach((cacheName) => {
+            const {
+              resultCount,
+              getCount,
+              getMeanTotal,
+              setCount,
+              setMeanTotal,
+            } = cacheNameResults[cacheName];
+            const getMean = Math.floor(getMeanTotal / resultCount);
+            const setMean = Math.floor(setMeanTotal / resultCount);
+            group.push([
+              workload,
+              parseInt(cacheSizeString, 10).toLocaleString(),
+              cacheName,
+              getMean.toLocaleString(),
+              setMean.toLocaleString(),
+              getCount * getMean + setCount * setMean,
+            ]);
+          });
+        const maxTotal = Math.max(
+          ...group.map((data) => data[data.length - 1])
+        );
+        group.forEach((data) => {
+          const totalTime = data[data.length - 1];
+          data[data.length - 1] = totalTime.toLocaleString();
+          data.push(((totalTime / maxTotal) * 100).toFixed(2));
+          console.log(`|${data.join("|")}|`);
+        });
+      });
+  });
